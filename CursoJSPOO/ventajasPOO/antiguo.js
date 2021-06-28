566cdad114b54d310840d2eb0edb7a632e7a6553
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-28 23:29:19 +0200

    Cambiando de archivo la zona comentada

---
+++ CursoJSPOO/ventajasPOO/antiguo.js
@@ -0,0 +1,64 @@
+const juan1 = {
+  name: "JuanDC",
+  username: "juandc",
+  points: 100,
+  socialMedia: {
+    twitter: "fjuandc",
+    instagram: "fjuandc",
+    facebook: undefined
+  },
+  approvedCourses: [
+    "Curso Definitivo de HTML y CSS",
+    "Curso Práctico de HTML y CSS"
+  ],
+  learningPaths: [
+    {
+      name: "Escuela de Desarrollo Web",
+      courses: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+        "Curso de Responsive Design"
+      ]
+    },
+    {
+      name: "Escuela de Videojuegos",
+      courses: [
+        "Curso de Introducción a la Producción de Videojuegos",
+        "Curso de Unreal Engine",
+        "Curso de Unity 3D"
+      ]
+    }
+  ]
+};
+const miguelito1 = {
+  name: "Miguelito",
+  username: "miguelitofeliz",
+  points: 1000,
+  socialMedia: {
+    twitter: "miguelitofeliz",
+    instagram: "miguelito_feliz",
+    facebook: undefined
+  },
+  approvedCourses: [
+    "Curso de DataBusiness",
+    "Curso de DataViz"
+  ],
+  learningPaths: [
+    {
+      name: "Escuela de Desarrollo Web",
+      courses: [
+        "Curso Definitivo de HTML y CSS",
+        "Curso Práctico de HTML y CSS",
+        "Curso de Responsive Design"
+      ]
+    },
+    {
+      name: "Escuela de Data Science",
+      courses: [
+        "Curso de DataBusiness",
+        "Curso de DataViz",
+        "Curso de Tableau"
+      ]
+    }
+  ]
+};
