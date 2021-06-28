684eb914d7acce562be8999c0553a102c9ebde9c
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-28 17:06:47 +0200

    Clases (Sintaxis distinta de prototipos)

--- CursoJSPOO/clase1/main.js
+++ CursoJSPOO/clase1/main.js
@@ -28,3 +28,28 @@ juanita.aprobarCurso( "Curso de Unreal Engine" );
 
 
 // Prototipos con sintaxis de clases
+class Student2 {
+  constructor ( {
+    name,
+    age,
+    cursosAprobados = [],
+    email
+  } ) {
+    this.name            = name;
+    this.age             = age;
+    this.cursosAprobados = cursosAprobados; 
+  }
+  aprobarCurso( nuevoCursito ) {
+    this.cursosAprobados.push(nuevoCursito);
+  }
+}
+// Al utilizar un objeto literal como parametro de la función constructora, podemos darle los valores desordenados o no darle todos. Para darle un valor por defecto, se le asigna en el objeto literal que es parámetro de la función constructora
+const miguelito = new Student2({
+  name: "Miguel",
+  age: 28,
+  email: "miguelita@miguel.com",
+  cursosAprobados: [
+    "Curso de Análisis de negocio para ciencia de datos",
+    "Curso de Principios de visualización de datos para BI"
+  ]
+});
