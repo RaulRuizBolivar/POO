4a8e1a899c4d9269359eadd92fdc776146b011db
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-28 23:25:37 +0200

    Abstracción

--- CursoJSPOO/ventajasPOO/main.js
+++ CursoJSPOO/ventajasPOO/main.js
@@ -62,6 +62,53 @@ const miguelito1 = {
     }
   ]
 };*/
+class Class {
+  constructor ( {
+    name
+  } )
+}
+class Course {
+  constructor ( {
+    name,
+    clases = [],
+    comments = []
+  } ) {
+    this.name   = name;
+    this.clases = clases;
+  }
+}
+const cursoProgramaciónBasica = new Course ( {
+  name: "Curso de programación básica"
+} );
+const cursoDefinitivoHTML = new Course ( {
+  name: "Curso definitivo de HTML y CSS"
+} );
+const cursoPracticoHTML = new Course ( {
+  name: "Curso Práctico de HTML y CSS"
+} );
+const cursoResponsiveDesign = new Course ( {
+  name: "Curso de Responsive Design"
+} );
+const cursoIntroduccionVgs = new Course ( {
+  name: "Curso de Introducción a la producción de videojuegos"
+} );
+const cursoUnreal = new Course ( {
+  name: "Curso de Unreal Engine"
+} );
+const cursoUnity = new Course ( {
+  name: "Curso de Unity 3D"
+} );
+const cursoDataBusiness = new Course ( {
+  name: "Curso de DataBusiness"
+} );
+const cursoDataViz = new Course ( {
+  name: "Curso de DataViz"
+} );
+const cursoTableau = new Course ( {
+  name: "Curso de Tableau"
+} );
+
+
 class LearningPath {
   constructor ( {
     name,
@@ -74,6 +121,33 @@ class LearningPath {
       this.courses.push( nuevoCurso );
     };
 }
+const escuelaWeb = new LearningPath( {
+  name: "Escuela de desarrollo web",
+  courses: [
+    cursoProgramaciónBasica,
+    cursoDefinitivoHTML,
+    cursoPracticoHTML,
+    cursoResponsiveDesign
+  ]
+} );
+const escuelaVgs = new LearningPath( {
+  name: "Escuela de desarrollo de videojuegos",
+  courses: [
+    cursoProgramaciónBasica,
+    cursoIntroduccionVgs,
+    cursoUnity,
+    cursoUnreal
+  ]
+} );
+const escuelaData = new LearningPath( {
+  name: "Escuela de Data Science",
+  courses: [
+    cursoProgramaciónBasica,
+    cursoDataBusiness,
+    cursoDataViz,
+    cursoTableau
+  ]
+} );
 class Student {
   constructor ( {
     name,
@@ -97,30 +171,7 @@ class Student {
     this.learningPaths = learningPaths;
   }
 }
-const escuelaWeb = new LearningPath( {
-  name: "Escuela de desarrollo web",
-  courses: [
-    "Curso Definitivo de HTML y CSS",
-    "Curso Práctico de HTML y CSS",
-    "Curso de Responsive Design"
-  ]
-} );
-const escuelaVgs = new LearningPath( {
-  name: "Escuela de desarrollo de videojuegos",
-  courses: [
-    "Curso de Introducción a la Producción de Videojuegos",
-    "Curso de Unreal Engine",
-    "Curso de Unity 3D"
-  ]
-} );
-const escuelaData = new LearningPath( {
-  name: "Escuela de Data Science",
-  courses: [
-    "Curso de DataBusiness",
-    "Curso de DataViz",
-    "Curso de Tableau"
-  ]
-} );
+
 const juan2 = new Student ( {
   name: "JuanDC",
   username: "juandc",
