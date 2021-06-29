b4445efb0c9c8ab4236deaf0cda0528a42f54f8f
Raúl Ruiz Bolivar <raul.ruiz.bolivar@gmail.com>
2021-06-29 22:42:27 +0200

    Modulos y encapsulamiento

---
+++ CursoJSPOO/ventajasPOO/module.mjs
@@ -0,0 +1,11 @@
+import { Class } from "./main.mjs";
+const clase67 = new Class( {
+  name: "JavaScript orieentado a objetos, basado en prototipos",
+  videoID: "cg23d5094r6my09384ryi'0mk"
+} );
+clase67.reproducir();
+clase67.pausar()
+clase67.reproducir();
+clase67.pausar();
+clase67.reproducir();
+clase67.pausar();
